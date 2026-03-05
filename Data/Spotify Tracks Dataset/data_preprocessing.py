f98273479ef86ff514a85db9bce4cba697311892
zeynepyavuz1621 <zeynepyavuz1621@gmail.com>
2026-03-05 17:56:10 +0300

    data_preprocessing.py file updated.

--- Data/Spotify Tracks Dataset/data_preprocessing.py
+++ Data/Spotify Tracks Dataset/data_preprocessing.py
@@ -5,4 +5,21 @@ from sklearn.preprocessing import MinMaxScaler
 df = pd.read_csv('dataset.csv')
 
 # Look at the raw data ranges
-print(df.describe())
+#print(df.describe())
+
+print(f"Original shape: {df.shape}")
+# 1. Drop duplicates based on artist and track_name
+df = df.drop_duplicates(subset=['artists', 'track_name'])
+# 2. Drop rows with missing values
+df = df.dropna()
+print(f"T1 shape: {df.shape}")
+df = df[(df['duration_ms'] >= 60000) & (df['duration_ms'] <= 600000)]
+print(f"T2 shape: {df.shape}")
+
+df['combined_features'] = (
+df['artists'].fillna('') + ' ' +
+df['album_name'].fillna('') + ' ' +
+df['track_name'].fillna('')
+)
+
+print(df['combined_features'].head)
